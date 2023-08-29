b70b853feb218241bd24db0c4b4d10b71956e4e5
codingslave2 <codingslavekim121@gmail.com>
2023-08-29 18:30:27 +0900

    20230829

--- src/main/java/Controller/MemberController.java
+++ src/main/java/Controller/MemberController.java
@@ -1,5 +1,23 @@
 package Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class MemberController {
+
+    @PostMapping("/register")
+    public String register(User user, @RequestParam String action) {
+
+        if ("회원 가입".equals(action)) {
+            // 회원 가입 로직
+            // 예: 데이터베이스에 사용자 정보 저장
+        } else if ("취소".equals(action)) {
+            // 취소 버튼 클릭 시 로직
+            return "redirect:/main";       }
+
+        return "registerForm";
+    }
 
 }
